b04d08835d95c119702a06240f8761039c1af89b
arnauddrm <arnaud.drm10@gmail.com>
2019-11-07 17:05:05 +0100

    07/11/2019

--- js/script.js
+++ js/script.js
@@ -46,23 +46,31 @@ function move(element, direction) {
             break;
     }
 }
+
+
+
+
 // event listener pour écouter les touches du clavier, et bouger le carré en fonction de ces touches
-window.addEventListener("keydown", function(event) {
+window.addEventListener("keydown", function (event) {
     switch (event.keyCode) {
         case 38: //code quand on va en haut
             move(carre, "haut");
+            carre.src = "img/bomber_haut.gif"
             break;
-        //droite
+            //droite
         case 39:
             move(carre, "droite");
+            carre.src = "img/bomber_droite.gif"
             break;
-        //bas
+            //bas
         case 40:
             move(carre, "bas");
+            carre.src = "img/bomber_bas.gif"
             break;
-        //gauche
+            //gauche
         case 37:
             move(carre, "gauche");
+            carre.src = "img/bomber_gauche.gif"
             break;
     }
     for (let i = 0; i < ennemi.length; i++) {
@@ -70,16 +78,26 @@ window.addEventListener("keydown", function(event) {
         let hautCarre = window.getComputedStyle(carre).getPropertyValue("top");
         let gaucheEnnemi = window.getComputedStyle(ennemi[i]).getPropertyValue("left");
         let hautEnnemi = window.getComputedStyle(ennemi[i]).getPropertyValue("top");
+        
+        // ICI -> on a essayé de faire perdre des vies et faire marcher le compteur aussi, quand on touche un ennemi, on a donc essayé d'appliquer la même chose qu'avec les bombes mais ca marche pas.
         if (gaucheCarre == gaucheEnnemi && hautCarre == hautEnnemi) {
-            window.setTimeout(function() {
+            window.setTimeout(function () {
                 cadre.style.backgroundColor = "lightgrey";
-            }, 200);
-            cadre.style.backgroundColor = "black";
+            }, 
+            200);
+            cadre.style.backgroundColor = "black"
+            // let life = 5;
+            //     let vie2 = document.querySelector(".vie")
+            // life -= 1;
+            // vie2.innerHTML = life
+            // console.log(life);
+            
+            carre.src = "img/bomber_touch.gif"
         }
     }
 });
 
-setInterval(function() {
+setInterval(function () {
     ennemi = document.querySelectorAll(".ennemi");
     for (let i = 0; i < ennemi.length; i++) {
         let random = getRandomInt(4);
@@ -87,13 +105,16 @@ setInterval(function() {
             case 0:
                 if (parseInt(window.getComputedStyle(ennemi[i]).getPropertyValue("top")) === 0) {
                     move(ennemi[i], "bas");
+                    // ennemi.src = "img/mechant_bas.gif"
                 } else {
                     move(ennemi[i], "haut");
+                    ennemi.src = "img/mechant_haut.gif"
                 }
                 break;
             case 1:
                 if (parseInt(window.getComputedStyle(ennemi[i]).getPropertyValue("top")) === 650) {
                     move(ennemi[i], "haut");
+                    // ennemi.src = "img/mechant_haut.gif"
                 } else {
                     move(ennemi[i], "bas");
                 }
@@ -118,7 +139,7 @@ setInterval(function() {
         let gaucheEnnemi = window.getComputedStyle(ennemi[i]).getPropertyValue("left");
         let hautEnnemi = window.getComputedStyle(ennemi[i]).getPropertyValue("top");
         if (gaucheCarre == gaucheEnnemi && hautCarre == hautEnnemi) {
-            window.setTimeout(function() {
+            window.setTimeout(function () {
                 cadre.style.backgroundColor = "lightgrey";
             }, 200);
             cadre.style.backgroundColor = "black";
@@ -136,19 +157,19 @@ function creerBombe() {
     cadre.appendChild(bombe);
     bombe.style.top = window.getComputedStyle(carre).getPropertyValue("top");
     bombe.style.left = window.getComputedStyle(carre).getPropertyValue("left");
-    window.setTimeout(function() {
+    window.setTimeout(function () {
         bombe.classList.add("explosion");
         bombe.innerText = "BOOM!!";
     }, 3000);
-    window.setTimeout(function() {
+    window.setTimeout(function () {
         bombe.classList.remove("explosion");
     }, 6000);
-    window.setTimeout(function() {
+    window.setTimeout(function () {
         cadre.removeChild(bombe);
     }, 6000);
 }
 
-window.addEventListener("keydown", function(e) {
+window.addEventListener("keydown", function (e) {
     let nbBombes = document.querySelectorAll(".bombe").length;
     if (e.keyCode == 32 && nbBombes < 3) {
         creerBombe();
@@ -168,7 +189,7 @@ function collision(bombe, perso) {
     }
 }
 
-window.setInterval(function() {
+window.setInterval(function () {
     for (let i = 0; i < ennemi.length; i++) {
         let bombes = document.querySelectorAll(".bombe");
         for (let j = 0; j < bombes.length; j++) {
@@ -183,14 +204,35 @@ window.setInterval(function() {
     }
 }, 100);
 
-window.setInterval(function() {
+let life = 5;
+let vie = document.querySelector(".vie")
+
+
+let kill_interval = window.setInterval(function () {
     let bombes = document.querySelectorAll(".bombe");
-    for (let j = 0; j < bombes.length; j++) {
-        if (bombes[j].classList.contains("explosion") && collision(bombes[j], carre)) {
-            window.setTimeout(function() {
-                cadre.style.backgroundColor = "lightgrey";
-            }, 200);
-            cadre.style.backgroundColor = "chartreuse";
+
+    if (life <= 0) {
+        clearInterval(kill_interval);
+        for (let i = 0; i < bombes.length; i++) {
+            bombes[i].remove();
+        }
+        carre.src = "img/dead.gif";
+        window.setTimeout(function () {
+            cadre.removeChild(carre);
+            document.querySelector(".got").append("GAME OVER");
+        }, 1500);
+    }
+    else {
+        for (let j = 0; j < bombes.length; j++) {
+            if (bombes[j].classList.contains("explosion") && collision(bombes[j], carre)) {
+                window.setTimeout(function () {
+                    vie.innerHTML = life
+                    life--;
+                    cadre.style.backgroundColor = "lightgrey";
+                }, 200);
+                cadre.style.backgroundColor = "chartreuse";
+                carre.src = "img/bomber_touch.gif"
+            };
         }
     }
-}, 100);
+}, 125);
